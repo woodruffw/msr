7f275ffc9732f680aca93a67c3f108be51d300b6
William Woodruff <william@tuffbizz.com>
2016-10-21 02:24:21 -0400

    lib: Add MSR::MSR505C as alias to MSR::MSR206.

--- lib/msr.rb
+++ lib/msr.rb
@@ -2,6 +2,7 @@ require_relative "../ext/msr/msr"
 require_relative "msr/track"
 require_relative "msr/tracks"
 require_relative "msr/msr206"
+require_relative "msr/msr505c"
 
 module MSR
   VERSION = "0.0.2".freeze
---
+++ lib/msr/msr505c.rb
@@ -0,0 +1,5 @@
+module MSR
+  # Alias MSR505C to MSR206, since the two are firmware compatible.
+  # @see MSR206
+  MSR505C = MSR206
+end
