bc0c2058719552ad5f9a9ec1e38033c1b110c2cc
William Woodruff <william@tuffbizz.com>
2016-11-02 17:38:49 -0400

    test: Add tests for BPI/BPC setters.

--- test/test_msr206.rb
+++ test/test_msr206.rb
@@ -44,11 +44,53 @@ class MSRMSR206Test < Minitest::Test
   end
 
   def test_msr206_bpi
-    skip # TODO
+    msr = MSR::MSR206.new("/dev/ttyUSB0")
+
+    # these should succeed, and are the only valid BPI values
+    msr.bpi = 75
+    msr.bpi = 210
+
+    # MSR206#bpi= only likes Fixnums
+    assert_raises TypeError do
+      msr.bpi = "what would this even do"
+    end
+
+    # only 75 and 210 are valid BPIs
+    assert_raises RuntimeError do
+      msr.bpi = 74
+    end
+
+    assert_raises RuntimeError do
+      msr.bpi = -1
+    end
   end
 
   def test_msr206_bpc
-    skip # TODO
+    msr = MSR::MSR206.new("/dev/ttyUSB0")
+
+    # these should succeed
+    msr.bpc = [5, 5, 5]
+    msr.bpc = [6, 6, 6]
+    msr.bpc = [7, 7, 7]
+    msr.bpc = [8, 8, 8]
+
+    # MSR206#bpc= only likes Arrays of 3 Fixnums
+    assert_raises TypeError do
+      msr.bpc = 10
+    end
+
+    assert_raises TypeError do
+      msr.bpc = [1, 2, 'not a number']
+    end
+
+    assert_raises ArgumentError do
+      msr.bpc = [1, 2, 3, 4]
+    end
+
+    # only 5, 6, 7, and 8 are valid BPC values for each track
+    assert_raises RuntimeError do
+      msr.bpc = [9, 9, 9]
+    end
   end
 
   def test_msr206_led
